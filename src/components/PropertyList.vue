b4d6793e3dce48f1ccf792a038e0e9b13ef62c45
qesnault <quentin.esnault@lilo.org>
2022-10-18 18:49:53 +0200

    debuts ui

---
+++ src/components/PropertyList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="overflow-x-auto">
+    <table class="min-w-full divide-y divide-gray-200 text-sm">
+      <thead class="bg-gray-100">
+        <tr>
+          <th
+              v-for="name in ['DESTINATION', 'STATUS', 'LOYER']"
+              class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
+          >
+           {{ name }}
+          </th>
+        </tr>
+      </thead>
+      <tbody class="divide-y divide-gray-200">
+      <tr>
+        <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+          <img src="https://source.unsplash.com/random/?apartment" alt="Un appartement" class="object-cover h-24">
+        </td>
+        <td class="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
+        <td class="whitespace-nowrap px-4 py-2 text-gray-700">Web Developer</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import PropertyList from "@/components/PropertyList.vue";
 </script>
 
 <template>
   <main>
-    <div class="text-3xl font-bold underline">
-      <h1>TABLEAU DE BOARD</h1>
-      <button>Ajouter un bien</button>
+    <div class="w-10/12 m-auto mt-6">
+      <div class="flex w-full justify-between mb-12">
+        <h1 class="text-2xl">TABLEAU DE BOARD</h1>
+        <button class="rounded bg-black px-5 py-3 text-white hover:bg-gray-800">Ajouter un bien</button>
+      </div>
+      <PropertyList />
+
     </div>
   </main>
 </template>
