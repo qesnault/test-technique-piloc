578a8733e59293d2490120b61a99fc0d28012f77
qesnault <quentin.esnault@lilo.org>
2022-10-18 18:31:52 +0200

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="text-3xl font-bold underline">
+      <h1>TABLEAU DE BOARD</h1>
+      <button>Ajouter un bien</button>
+    </div>
+  </main>
+</template>
